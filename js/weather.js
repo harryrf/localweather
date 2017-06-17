18e2aa986a2ae75f3c9f6a599b452419af4a752c
Harry F <harryrf3@gmail.com>
2017-06-17 12:16:49 -0400

    Head exploded

--- js/weather.js
+++ js/weather.js
@@ -10,24 +10,29 @@ $(document).ready(function () {
 
   // location API
   $.getJSON("https://ipinfo.io/json", function (location) {
-
     var coords = location.loc;
+
+    // Live API
     var api = "https://api.apixu.com/v1/forecast.json?key=49f97e05d5144c9c829185418170806&q=" + coords + "&days=3";
+
+    // Dev API
+    var devapi = "http://192.168.0.2:8080/json/dev-3-day-forecast.json";
 
     // coordinates gathered from ipinfo.io
     // it's more precise
     $("#userLoc").html(location.city + ", " + location.region);
 
     // weather API
-    $.getJSON(api, function (geo) {
+    $.getJSON(devapi, function (geo) {
+
 
       var location = geo.location;
       var current = geo.current;
       var condition = geo.current.condition;
       var forecastday = geo.forecast.forecastday[0].day;
-
       var is_day = geo.current.is_day;
       var cCode = geo.current.condition.code;
+
       var icon = "";
 
       // check how much total precipitation will
@@ -48,24 +53,24 @@ $(document).ready(function () {
         switch (true) {
 
           case cCode == 1000:
-            icon = "climacon sun";
+            icon = "climacon sun iconMedium";
             break;
 
           case cCode == 1003:
-            icon = "climacon cloud sun";
+            icon = "climacon cloud sun iconMedium";
             break;
 
           case cCode == 1006:
-            icon = "climacon cloud";
+            icon = "climacon cloud iconMedium";
             break;
 
           case cCode == 1009:
-            icon = "climacon haze sun";
+            icon = "climacon haze sun iconMedium";
             break;
 
           case cCode == 1030:
           case cCode == 1135:
-            icon = "climacon fog sun";
+            icon = "climacon fog sun iconMedium";
             break;
 
           case cCode == 1063:
@@ -74,7 +79,7 @@ $(document).ready(function () {
           case cCode == 1240:
           case cCode == 1243:
 
-            icon = "climacon rain sun";
+            icon = "climacon rain sun iconMedium";
             break;
 
           case cCode == 1066:
@@ -83,7 +88,7 @@ $(document).ready(function () {
           case cCode == 1222:
           case cCode == 1255:
           case cCode == 1258:
-            icon = "climacon snow sun";
+            icon = "climacon snow sun iconMedium";
             break;
 
           case cCode == 1069:
@@ -93,79 +98,79 @@ $(document).ready(function () {
           case cCode == 1252:
           case cCode == 1261:
           case cCode == 1264:
-            icon = "climacon sleet sun";
+            icon = "climacon sleet sun iconMedium";
             break;
 
           case cCode == 1072:
           case cCode == 1198:
           case cCode == 1201:
             // needs climacon thermometer
-            icon = "climacon rain";
+            icon = "climacon rain iconMedium";
             break;
 
           case cCode == 1186:
           case cCode == 1189:
-            icon = "climacon rain";
+            icon = "climacon rain iconMedium";
             break;
 
           case cCode == 1087:
           case cCode == 1273:
-            icon = "climacon lightning sun";
+            icon = "climacon lightning sun iconMedium";
             break;
 
           case cCode == 1279:
             // needs climacon snowflake
-            icon = "climacon lightning sun";
+            icon = "climacon lightning sun iconMedium";
             break;
 
           case cCode == 1282:
             // needs climacon snowflake
-            icon = "climacon lightning";
+            icon = "climacon lightning iconMedium";
             break;
 
           case cCode == 1114:
             // needs climacon wind
-            icon = "climacon snow";
+            icon = "climacon snow iconMedium";
             break;
 
           case cCode == 1117:
           case cCode == 1213:
           case cCode == 1219:
           case cCode == 1225:
-            icon = "climacon snow";
+            icon = "climacon snow iconMedium iconMedium";
             break;
 
           case cCode == 1147:
             // needs climacon thermometer
-            icon = "climacon fog";
+            icon = "climacon fog iconMedium";
             break;
 
           case cCode == 1150:
           case cCode == 1153:
-            icon = "climacon drizzle sun";
+            icon = "climacon drizzle sun iconMedium";
             break;
 
           case cCode == 1168:
           case cCode == 1171:
             // needs climacon thermometer
-            icon = "climacon drizzle";
+            icon = "climacon drizzle iconMedium";
             break;
 
           case cCode == 1192:
           case cCode == 1246:
-            icon = "climacon downpour sun";
+            icon = "climacon downpour sun iconMedium";
             break;
 
           case cCode == 1195:
-            icon = "climacon downpour";
+            icon = "climacon downpour iconMedium";
             break;
 
           case cCode == 1237:
-            icon = "climacon sleet";
+            icon = "climacon sleet iconMedium";
             break;
 
           case cCode == 1276:
-            icon = "climacon lightning";
+            icon = "climacon lightning iconMedium";
             break;
 
         }
@@ -173,24 +178,24 @@ $(document).ready(function () {
         switch (true) {
 
           case cCode == 1000:
-            icon = "climacon moon";
+            icon = "climacon moon iconLarge";
             break;
 
           case cCode == 1003:
-            icon = "climacon cloud moon";
+            icon = "climacon cloud moon iconMedium";
             break;
 
           case cCode == 1006:
-            icon = "climacon cloud";
+            icon = "climacon cloud iconMedium";
             break;
 
           case cCode == 1009:
-            icon = "climacon haze moon";
+            icon = "climacon haze moon iconMedium";
             break;
 
           case cCode == 1030:
           case cCode == 1135:
-            icon = "climacon fog moon";
+            icon = "climacon fog moon iconMedium";
             break;
 
           case cCode == 1063:
@@ -199,7 +204,7 @@ $(document).ready(function () {
           case cCode == 1240:
           case cCode == 1243:
 
-            icon = "climacon rain moon";
+            icon = "climacon rain moon iconMedium";
             break;
 
           case cCode == 1066:
@@ -208,7 +213,7 @@ $(document).ready(function () {
           case cCode == 1222:
           case cCode == 1255:
           case cCode == 1258:
-            icon = "climacon snow moon";
+            icon = "climacon snow moon iconMedium";
             break;
 
           case cCode == 1069:
@@ -218,87 +223,89 @@ $(document).ready(function () {
           case cCode == 1252:
           case cCode == 1261:
           case cCode == 1264:
-            icon = "climacon sleet moon";
+            icon = "climacon sleet moon iconMedium";
             break;
 
           case cCode == 1072:
           case cCode == 1198:
           case cCode == 1201:
             // needs climacon thermometer
-            icon = "climacon rain";
+            icon = "climacon rain iconMedium";
             break;
 
           case cCode == 1186:
           case cCode == 1189:
-            icon = "climacon rain";
+            icon = "climacon rain iconMedium";
             break;
 
           case cCode == 1087:
           case cCode == 1273:
-            icon = "climacon lightning moon";
+            icon = "climacon lightning moon iconMedium";
             break;
 
           case cCode == 1279:
             // needs climacon snowflake
-            icon = "climacon lightning moon";
+            icon = "climacon lightning moon iconMedium";
             break;
 
           case cCode == 1282:
             // needs climacon snowflake
-            icon = "climacon lightning";
+            icon = "climacon lightning iconMedium";
             break;
 
           case cCode == 1114:
             // needs climacon wind
-            icon = "climacon snow";
+            icon = "climacon snow iconMedium";
             break;
 
           case cCode == 1117:
           case cCode == 1213:
           case cCode == 1219:
           case cCode == 1225:
-            icon = "climacon snow";
+            icon = "climacon snow iconMedium";
             break;
 
           case cCode == 1147:
             // needs climacon thermometer
-            icon = "climacon fog";
+            icon = "climacon fog iconMedium";
             break;
 
           case cCode == 1150:
           case cCode == 1153:
-            icon = "climacon drizzle moon";
+            icon = "climacon drizzle moon iconMedium";
             break;
 
           case cCode == 1168:
           case cCode == 1171:
             // needs climacon thermometer
-            icon = "climacon drizzle";
+            icon = "climacon drizzle iconMedium";
             break;
 
           case cCode == 1192:
           case cCode == 1246:
-            icon = "climacon downpour moon";
+            icon = "climacon downpour moon iconMedium";
             break;
 
           case cCode == 1195:
-            icon = "climacon downpour";
+            icon = "climacon downpour iconMedium";
             break;
 
           case cCode == 1237:
-            icon = "climacon sleet";
+            iconMedium
+            icon = "climacon sleet iconMedium";
             break;
 
           case cCode == 1276:
-            icon = "climacon lightning";
+            icon = "climacon lightning iconMedium";
             break;
         }
       }
 
+      $("#test").html(condition.text);
       // Current top
       $("#currentConditionText").html(condition.text);
-      $("#currentConditionIcon").append("<div class='climaLarge " + icon + "'></div>");
-      $("#temp").html(current.temp_f + "&#8457;");
+      $("#currentConditionIcon").append("<div class='" + icon + "'></div>");
+      $("#currentTemp").html(current.temp_f + "&#8457;");
 
       // Current bottom
 
